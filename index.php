7edfaedfd06f9081c50254f2b3b15b12f888c1ab
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-18 14:19:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+</head>
+<body>
+    <?php
+    foreach($hotels as $hotel){
+        foreach($hotel as $key => $value){
+            echo ucfirst($key) . " :";
+            if($key == "parking"){
+                if($value == true){
+                    echo " Yes";
+                }else{
+                    echo " No";
+                }
+            }else{
+                echo " $value";
+            }
+            echo "<br>";
+        }
+        echo "<hr>";
+    }   
+    ?>
+</body>
+</html>
