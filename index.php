f6f468a915f40838f1921674ecb304d4687e5726
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-18 15:30:22 +0200

    [UPD] Add data to table

--- index.php
+++ index.php
@@ -43,27 +43,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Hotel</title>
 </head>
-<body>
-    <?php
-    foreach($hotels as $hotel){
-        foreach($hotel as $key => $value){
-            //Print key
-            echo str_replace("_", " ", ucfirst($key)) . " :";
-            if($key == "parking"){
-                if($value == true){
-                    echo " Yes";
-                }else{
-                    echo " No";
-                }
-            }else{
-                echo " $value";
-            }
-            echo "<br>";
-        }
-        echo "<hr>";
-    }   
-    ?>
+    <body class="vh-100 d-flex">
+    <h1></h1>
+        <table style="width: 50%;" class="m-auto table table-bordered">
+            <thead>
+                <?php 
+                    echo "<tr>";
+                        foreach($hotels[0] as $key => $hotel){
+                            echo "<th>" . str_replace("_", " ", ucfirst($key)) . "</th>";
+                        };
+                    echo "</tr>";
+                ?>
+            </thead>
+            <tbody>
+                <?php 
+                    foreach($hotels as $hotel){
+                        echo "<tr>";
+                            foreach($hotel as $key => $value){
+                                if($key == "parking"){
+                                    if($value == true){
+                                        echo "<td> Yes </td>";
+                                    }else{
+                                        echo "<td> No </td>";
+                                    }
+                                }else{
+                                    echo "<td> $value </td>";
+                                }
+                            };
+                        echo "</tr>";
+                    };
+                ?>
+            </tbody>
+        </table>
+            
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
